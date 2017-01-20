2443988c9346efb3777cee36b7eb5b64fb0b2a78
sharp <simon.sharp@gmx.ch>
2017-01-20 23:19:55 +0100

    Add Enemy Script

--- Fluctus/Assets/Scripts/EnemyWaveCollisionScript.cs
+++ Fluctus/Assets/Scripts/EnemyWaveCollisionScript.cs
@@ -2,30 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyWaveCollisionScript : MonoBehaviour {
-    GameObject[] waves;
-
-    // Use this for initialization
-    void Start () {
-        waves = GameObject.FindGameObjectsWithTag("Wave");
-        Debug.Log("adsf");
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+public class EnemyWaveCollisionScript : MonoBehaviour
+{
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log(coll.gameObject.tag);
+        Debug.Log(coll.collider.gameObject);
 
-        if (coll.gameObject.tag == "WaveInnerCollider")
+        if (coll.collider.gameObject.tag == "WaveInnerCollider")
         {
             Debug.Log("Enter Inner Collision");
         }
 
-        if (coll.gameObject.tag == "WaveOuterCollider")
+        if (coll.collider.gameObject.tag == "WaveOuterCollider")
         {
             Debug.Log("Enter Outer Collision");
         }
---
+++ Fluctus/Assets/Scripts/WaveCollisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveCollisionScript : MonoBehaviour
+{
+    public float grothRatio = 0.01f;
+    CircleCollider2D circleCollider;
+
+    public float waveStartRadius = 1.0f;
+
+    public float deleteWhenBiggerThan = 10.0f;
+
+    void Start()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+
+        circleCollider.radius = waveStartRadius;
+    }
+
+    void FixedUpdate()
+    {
+        circleCollider.radius += this.grothRatio;
+
+        if (circleCollider.radius > deleteWhenBiggerThan)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
