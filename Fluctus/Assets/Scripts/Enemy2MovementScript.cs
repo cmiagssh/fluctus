753ebc5d51f65d3e68a5ec8fa11c3c5ffdb42b24
sharp <simon.sharp@gmx.ch>
2017-01-21 12:38:51 +0100

    Add Enemy2Movement Script

---
+++ Fluctus/Assets/Scripts/Enemy2MovementScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2MovementScript : MonoBehaviour {
+    GameObject closestPlayer;
+
+    void FixedUpdate()
+    {
+        GetClosestPlayerPosition();
+        RotateEnemy();
+        MoveEnemy();
+        DestroyEnemy();
+    }
+
+    void GetClosestPlayerPosition() {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player1");
+        GameObject player1 = players[0];
+
+        players = GameObject.FindGameObjectsWithTag("Player2");
+        GameObject player2 = players[0];
+
+        if (
+            Vector3.Distance(player1.transform.position, gameObject.transform.position) <
+            Vector3.Distance(player2.transform.position, gameObject.transform.position)
+        )
+        {
+            closestPlayer = player1;
+        }
+        else {
+            closestPlayer = player2;
+        }
+
+        Debug.Log(closestPlayer);
+    }
+
+    void RotateEnemy()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.realtimeSinceStartup) * 40);
+    }
+
+    void MoveEnemy()
+    {
+        transform.position += transform.right * Time.deltaTime;
+    }
+
+    void DestroyEnemy()
+    {
+        if (transform.position.x > 10.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
