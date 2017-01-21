00ede58b0d676ef1217b4e99d68579a1c40903ab
sharp <simon.sharp@gmx.ch>
2017-01-21 14:44:05 +0100

    expand enemy player script

--- Fluctus/Assets/Scripts/Enemy2MovementScript.cs
+++ Fluctus/Assets/Scripts/Enemy2MovementScript.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy2MovementScript : MonoBehaviour {
+<<<<<<< HEAD
+    public GameObject closestPlayer;
+
+    public float speed = 1000000000000.0f;
+
+    public Transform Target;
+
+    //values for internal use
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+=======
     GameObject closestPlayer;
+>>>>>>> 6cf131f8a596d41bb4ed28b29115f65a1a85cc1e
 
     void FixedUpdate()
     {
@@ -31,12 +43,30 @@ public class Enemy2MovementScript : MonoBehaviour {
             closestPlayer = player2;
         }
 
+<<<<<<< HEAD
+        Target = closestPlayer.transform;
+    }
+
+    void RotateEnemy()
+    {
+        //find the vector pointing from our position to the target
+        _direction = (Target.position - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        _lookRotation = Quaternion.LookRotation(_direction);
+
+        //rotate us over time according to speed until we are in the required rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime);
+
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+=======
         Debug.Log(closestPlayer);
     }
 
     void RotateEnemy()
     {
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.realtimeSinceStartup) * 40);
+>>>>>>> 6cf131f8a596d41bb4ed28b29115f65a1a85cc1e
     }
 
     void MoveEnemy()
@@ -46,10 +76,14 @@ public class Enemy2MovementScript : MonoBehaviour {
 
     void DestroyEnemy()
     {
+<<<<<<< HEAD
+
+=======
         if (transform.position.x > 10.0f)
         {
             Destroy(gameObject);
         }
+>>>>>>> 6cf131f8a596d41bb4ed28b29115f65a1a85cc1e
     }
 }
 
