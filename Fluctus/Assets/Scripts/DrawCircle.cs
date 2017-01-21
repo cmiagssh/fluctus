4251dac405d76e0c2ac3a2f8b47df0224c9a394d
jacasch <junk.u.spam@gmail.com>
2017-01-21 03:44:39 +0100

    Add Player Controls in Scene 2
    Added 2nd Player
    Added movememnt mechanic with thrust and roll
    Added Hack to bypas first (glitchy) frame of wave spawn
    changed wave texture

--- Fluctus/Assets/Scripts/DrawCircle.cs
+++ Fluctus/Assets/Scripts/DrawCircle.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class DrawCircle : MonoBehaviour {
 
     private LineRenderer lineRenderer;
-    private int circleSegments = 1024;
+    private int circleSegments = 64;
+    private Vector2 origin;
+    private int frameCount = 0;
 
     public float radius = 5;
     public float width = 1;
-    public Vector2 origin;
-
+    
 	// Use this for initialization
 	void Start ()
     {
+        origin = new Vector2(transform.position.x, transform.position.y);
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.numPositions = circleSegments + 1;
 
         //tweak this to round corners
         lineRenderer.numCornerVertices = 10;
 
+        lineRenderer.enabled = false;
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        radius += 0.1f;
         drawCircle();
+        if (frameCount > 1)
+        {
+            lineRenderer.enabled = true;
+        }
+        else
+        {
+            frameCount++;
+        }
+        
+        //check despawn
+        if (radius > 20f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void drawCircle ()
@@ -33,7 +52,7 @@ public class DrawCircle : MonoBehaviour {
         lineRenderer.startWidth = width;
         lineRenderer.endWidth = width;
         //for all vertecies on circle
-        for (int i = 0; i <= circleSegments + 1; i++)
+        for (int i = 0; i < circleSegments + 1; i++)
         {
             float angle = 2 * Mathf.PI / circleSegments * i;
             float x = origin.x + radius * Mathf.Cos(angle);
---
+++ Fluctus/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    private Rigidbody2D rigidBody;
+    private GameObject newWavePrefab;
+
+    public GameObject wavePrefab;
+    public string rollInput;
+    public string thrustInput;
+    public string fireInput;
+    public float thrust;
+
+    public float angularDrag;
+    public float linearDrag;
+    public float rollForce;
+    public float thrustForce;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.angularDrag = angularDrag;
+        rigidBody.drag = linearDrag;
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var roll = Input.GetAxis(rollInput);
+        thrust = Input.GetAxis(thrustInput);
+        //Vector3 direction = new Vector3(x, y, 0).normalized;
+        //transform.position += direction * speed * Time.deltaTime;
+
+        //thust
+        rigidBody.AddForce(transform.up * thrust * thrustForce);
+        rigidBody.angularVelocity = -(roll * rollForce);
+
+        //torque
+
+
+
+        if (Input.GetButtonDown(fireInput))
+        {
+            newWavePrefab = Instantiate(wavePrefab, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+        }
+    }
+}
